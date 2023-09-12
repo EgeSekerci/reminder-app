01269ec909a259d7ac67d0a6bcd2f7875f04a5e9
Ege <yegesekerci@gmail.com>
2023-09-12 07:47:36 +0300

    #3 Minor fixes

--- backend/models/reminderModel.js
+++ backend/models/reminderModel.js
@@ -14,6 +14,6 @@ const reminderSchema = new Schema({
     description: {
         type: String
     }
-}, { timestams: true })
+}, { timestamps: true })
 
 module.exports = mongoose.model('Reminder', reminderSchema)
